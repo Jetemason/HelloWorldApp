5df63d36b6c99c0e7148423eb6530e1e96a6ffa2
Igor Ilyin <53910111+Jetemason@users.noreply.github.com>
2020-01-28 22:38:17 +0500

    Initial Commit

---
+++ HelloWorldApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorldApp
+//
+//  Created by Игорь Ильин on 28.01.2020.
+//  Copyright © 2020 Igor Ilyin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
